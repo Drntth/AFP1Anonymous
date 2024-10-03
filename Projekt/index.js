7acbd7c45827d4d311fd91db4865d6052802791b
DMNK <plasku.dominik1217@gmail.com>
2024-10-03 14:12:52 +0200

    Updated Projekt - HTML-ben kategóriák classok létrehozása a szűréshez, JavaScriptben function létrehozása
    HTML: Navbaron keresztül a kategóriákra kattintva meghívja a javascriptben készített szűrési funkciót
    
    JavaScript: filterArticles funkció létrehozása, hozzáadása

--- Projekt/index.js
+++ Projekt/index.js
@@ -10,3 +10,26 @@ newsArticles.forEach(article => {
     });
 });
 
+// kategóriák megjelenítése
+function filterArticles(category) {
+    // Elrejti az összes kategóriát
+    var containers = document.querySelectorAll('.news-container');
+    containers.forEach(function(container) {
+        container.style.display = 'none';
+    });
+
+    // Ha 'all' van kiválasztva, megjelenít minden cikket
+    if (category === 'all') {
+        containers.forEach(function(container) {
+            container.style.display = 'block';
+        });
+    } else {
+        // Csak a kiválasztott kategória elemeit jeleníti meg
+        var selectedContainers = document.querySelectorAll('.' + category);
+        selectedContainers.forEach(function(container) {
+            container.style.display = 'block';
+        });
+    }
+}
+
+
